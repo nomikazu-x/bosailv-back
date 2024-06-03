ca8d5d465930b2fa05cd4fa3999f7935e8f3dca7
nomikazu <nomikazyu@gmail.com>
2024-06-03 12:23:49 +0900

    fix: flyにデプロイ

--- db/migrate/20220425140633_create_articles.rb
+++ db/migrate/20220425140633_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[6.1]
     create_table :articles do |t|
       t.references :user, null: false, foreign_key: true,  comment: 'ユーザーID'
       t.string :title, null: false, limit: 30,             comment: 'タイトル'
-      t.text :content, null: false, limit: 4294967295,     comment: '内容'
+      t.text :content, null: false,                        comment: '内容'
       t.timestamps
     end
   end
--- db/migrate/20220717031531_create_tasks.rb
+++ db/migrate/20220717031531_create_tasks.rb
@@ -4,7 +4,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
       t.text   :image,   null: false,                    comment: '画像'
       t.string :title,   null: false, limit: 30,         comment: 'タイトル'
       t.string :summary, null: false, limit: 50,         comment: '概要'
-      t.text   :body,    null: false, limit: 4294967295, comment: '本文'
+      t.text   :body,    null: false,                    comment: '本文'
       t.timestamps
     end
   end
